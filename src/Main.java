a8c120f45005e055758663defbd359644b20751d
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-12 17:11:39 +0200

    S'ha creat el mètode davide

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        davide();
+    }
+    private static void davide(){
+        System.out.println("Hello Davide!");
     }
 }
