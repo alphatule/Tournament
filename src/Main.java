4bba5758522236ea88bbc5f7530ff46e23c8945d
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-12 17:24:07 +0200

    S'ha creat el mètode alex, ja que no hi és a classe.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,6 @@ public class Main {
     }
 
     private static void nacho(){
-        System.out.println("Hello davide mamyema!");
+        System.out.println("Hello davide mamañema!");
     }
 }
