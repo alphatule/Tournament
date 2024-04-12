4f8c9887bc13a20ce255fa493239f243bd98558f
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-12 17:22:54 +0200

    S'ha creat el mètode alex, ja que no hi és a classe.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        alex();
         davide();
         nacho();
+    }
+    private static void alex(){
+        System.out.println("Hello Alex!");
     }
     private static void davide(){
         System.out.println("Hello Davide!");
