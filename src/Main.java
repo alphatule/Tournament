aa706d595090d43374821200900b67ada43687bd
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2024-04-12 17:24:37 +0200

    a.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        alex();
         davide();
         nacho();
-    }
-    private static void alex(){
-        System.out.println("Hello Alex!");
     }
     private static void davide(){
         System.out.println("Hello Davide!");
     }
 
     private static void nacho(){
-        System.out.println("Hello davide mamañema!");
+        System.out.println("Hello davide!");
     }
 }
